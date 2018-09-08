960a0fccdf6839ee205937dc943cb9f903f8db3c
azamat <azamat.murdalov@mail.ru>
2018-09-09 01:17:27 +0300

    update rage.py

--- rage.py
+++ rage.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 
-# Будем использовать Yandex Speachkit для синтеза речи
-# (ключ нам не понадобится)
-
-# импортируем модуль requests чтоб отправить get запрос
 import requests
-# импортируем модуль pyglet для воспроизведения mp3-файла
 import pyglet
 
-# формируем запрос, который вернет озвученный текст
-request = requests.get("https://tts.voicetech.yandex.net/tts?text=Hello World")
+url = "https://tts.voicetech.yandex.net/tts"
+speaker = "ermil"
 
-# открываем (создаем) mp3-файл в режиме 'wb' (write-binary-mode)
-# и записываем в него озвученный текст
-open('file.mp3', 'wb').write(request.content)
+# функция преобразовывает текст в речь, а затем воспроизводит
+def text_to_speech(text):
+    request = requests.get(url + "?text=" + text + "&speaker=" + speaker)
+    open('file.mp3', 'wb').write(request.content)
+    song = pyglet.media.load('file.mp3')
+    song.play()
+    pyglet.app.run()
 
-# воспроизводим наш mp3-файл
-song = pyglet.media.load('file.mp3')
-song.play()
-pyglet.app.run()
+text = raw_input(" -> ")
+
+if text == "ты робот?":
+    text_to_speech("А разве не видно?")
+else:
+    text_to_speech("Не понимаю")
