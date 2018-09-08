70350caa0082b25099d0b000e98ba35a6d481c4c
azamat <azamat.murdalov@mail.ru>
2018-09-08 21:23:52 +0300

    use Yandex SpeechKit Cloud

--- rage.py
+++ rage.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
-from gtts import gTTS
 
-voice = gTTS(text=u'Привет Азамат. Меня зовут Рэйдж', lang='ru')
-voice.save('voice_Rage.mp3')
+# Будем использовать Yandex Speachkit для синтеза речи
+# (ключ нам не понадобится)
+
+# импортируем модуль requests чтоб отправить get запрос
+import requests
+# импортируем модуль pyglet для воспроизведения mp3-файла
+import pyglet
+
+# формируем запрос, который вернет озвученный текст
+request = requests.get("https://tts.voicetech.yandex.net/tts?text=Hello World")
+
+# открываем (создаем) mp3-файл в режиме 'wb' (write-binary-mode)
+# и записываем в него озвученный текст
+open('file.mp3', 'wb').write(request.content)
+
+# воспроизводим наш mp3-файл
+song = pyglet.media.load('file.mp3')
+song.play()
+pyglet.app.run()
