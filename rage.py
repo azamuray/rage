8b38254955e805d70d0900d46296bec561007871
azamat <azamat.murdalov@mail.ru>
2018-09-09 20:59:08 +0300

    add pyaudio and wave modules

--- rage.py
+++ rage.py
@@ -1,22 +1,55 @@
 # -*- coding: utf-8 -*-
 
 import requests
-import pyglet
+import pyaudio
+import wave
 
-url = "https://tts.voicetech.yandex.net/tts"
-speaker = "ermil"
-
-# функция преобразовывает текст в речь, а затем воспроизводит
+# преобразовываем текст в речь и записываем в wav-файл
 def text_to_speech(text):
-    request = requests.get(url + "?text=" + text + "&speaker=" + speaker)
-    open('file.mp3', 'wb').write(request.content)
-    song = pyglet.media.load('file.mp3')
-    song.play()
-    pyglet.app.run()
+    url = 'https://tts.voicetech.yandex.net/tts'
+    get_file = requests.get(url + '?text=' + text + '&format=wav&speaker=ermil')
+    open('file.wav', 'wb').write(get_file.content)
 
-text = raw_input(" -> ")
+# воспроизводим полученный wav-файл c помощью pyaudio и wave
+def play():
 
-if text == "ты робот?":
-    text_to_speech("А разве не видно?")
-else:
-    text_to_speech("Не понимаю")
+    #задаем количество сэмплов
+    CHUNK = 1024
+
+    #открываем файл wav-формата
+    f = wave.open(r"file.wav","rb")
+
+    #создаем объект PyAudio
+    p = pyaudio.PyAudio()
+
+    #открываем поток (набор) данных
+    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
+                    channels = f.getnchannels(),
+                    rate = f.getframerate(),
+                    output = True)
+
+    data = f.readframes(CHUNK)
+
+    #запускаем поток
+    while data:
+        stream.write(data)
+        data = f.readframes(CHUNK)
+
+    #останавливаем поток
+    stream.stop_stream()
+    stream.close()
+
+    #закрываем PyAudio
+    p.terminate()
+
+while True:
+    text = raw_input(" -> ")
+
+    if text == "ты робот?":
+        text_to_speech("А разве не видно?")
+        play()
+        print "Что нибудь еще?"
+    else:
+        text_to_speech("Не понимаю")
+        play()
+        print "Что нибудь еще?"
