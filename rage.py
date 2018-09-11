cbd2599c1b69e4c7947ac85f5800351d095082e9
azamat <azamat.murdalov@mail.ru>
2018-09-11 06:34:09 +0300

    connecting database

--- rage.py
+++ rage.py
@@ -3,26 +3,27 @@
 import requests
 import pyaudio
 import wave
+import sqlite3
 
-# преобразовываем текст в речь и записываем в wav-файл
+# преобразовываю текст в речь и записываю в wav-файл
 def text_to_speech(text):
     url = 'https://tts.voicetech.yandex.net/tts'
     get_file = requests.get(url + '?text=' + text + '&format=wav&speaker=ermil')
     open('file.wav', 'wb').write(get_file.content)
 
-# воспроизводим полученный wav-файл c помощью pyaudio и wave
+# воспроизвожу полученный wav-файл c помощью pyaudio и wave
 def play():
 
-    #задаем количество сэмплов
+    # задаю количество сэмплов
     CHUNK = 1024
 
-    #открываем файл wav-формата
+    # открываю файл wav-формата
     f = wave.open(r"file.wav","rb")
 
-    #создаем объект PyAudio
+    # создаю объект PyAudio
     p = pyaudio.PyAudio()
 
-    #открываем поток (набор) данных
+    # открываю поток (набор) данных
     stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
                     channels = f.getnchannels(),
                     rate = f.getframerate(),
@@ -30,34 +31,33 @@ def play():
 
     data = f.readframes(CHUNK)
 
-    #запускаем поток
+    # запускаю поток
     while data:
         stream.write(data)
         data = f.readframes(CHUNK)
 
-    #останавливаем поток
+    # останавливаю поток
     stream.stop_stream()
     stream.close()
 
-    #закрываем PyAudio
+    # закрываю PyAudio
     p.terminate()
 
-# создаем базу команд
-base = ["Привет = Приветствую тебя, человек",
-        "Видишь меня? = Пока что не вижу, у меня глаз",
-        "Понимаешь меня? = Я робот, я не могу понять",
-        "Ты такой грустный = Роботы не бывают грустными",
-        "Ты робот? = Конечно робот"]
+# делаю соединение с базой данных
+connection = sqlite3.connect('base.db')
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM commands")
+rows = cursor.fetchall()
 
-# запускаем робота
-answer = "Я тебя не понимаю"
+# запускаю робота
 while True:
+    answer = "Я тебя не понимаю"
     text = raw_input(" -> ")
-    for comand in base:
-        comand = comand.split(" = ")
+    text = unicode(text, "UTF-8")
+    for comand in rows:
         if text == comand[0]:
             answer = comand[1]
-        elif text == "Выключись":
-            break
+    if text == u"Выключись":
+        break
     text_to_speech(answer)
     play()
