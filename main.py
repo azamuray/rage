e50ff2d011a85677077aae2ab19dc1a4f6481180
Rage <azamat.murdalov@mail.ru>
2021-08-08 22:46:36 +0300

    refactoring: add init to Rage class

--- main.py
+++ main.py
@@ -21,41 +21,46 @@ from db.models import Command
 session = Session()
 
 
-class Base():
+class Base:
+
+    model = Model("speech_to_text")
+    rec = KaldiRecognizer(model, 16000)
+    stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
+                                    channels=1,
+                                    rate=16000,
+                                    input=True,
+                                    frames_per_buffer=8000)
 
     def waiting(self, record):
         """Выключение микрофона во время воспроизведения."""
         pygame.mixer.init()
         seconds = pygame.mixer.Sound(record).get_length()
-        stream.stop_stream()
+        self.stream.stop_stream()
         time.sleep(seconds)
-        stream.start_stream()
+        self.stream.start_stream()
 
 
-class Voice():
-
-    base = Base()
+class Voice(Base):
 
     def add_answer(self):
         answer_record = 'voice/study.wav'
         module.play(answer_record)
-        self.base.waiting(answer_record)
+        self.waiting(answer_record)
 
         answer_record = 'voice/1.wav'
         module.play(answer_record)
-        self.base.waiting(answer_record)
+        self.waiting(answer_record)
 
         first = self.speech_record()
         print("Я услышал: ", first)
         answer_record = 'voice/2.wav'
         module.play(answer_record)
-        self.base.waiting(answer_record)
+        self.waiting(answer_record)
 
         second = self.speech_record()
         print("Я услышал: ", second)
 
         session.add(Command(question=first, answer=second))
-
 
     def get_answer(self, text):
         answer = "Я не понимаю"
@@ -68,84 +73,67 @@ class Voice():
         if exists:
             answer_record = 'records/%s.wav' % answer
             module.play(answer_record)
-            self.base.waiting(answer_record)
+            self.waiting(answer_record)
         else:
             module.text_to_speech(answer)
             answer_record = 'records/%s.wav' % answer
             module.play(answer_record)
-            self.base.waiting(answer_record)
+            self.waiting(answer_record)
             print('Я сохранил файл: %s.wav' % answer)
 
     def speech_record(self):
         while True:
-            data = stream.read(4000)
-            if rec.AcceptWaveform(data):
-                text = json.loads(rec.Result())['text']
+            data = self.stream.read(4000)
+            if self.rec.AcceptWaveform(data):
+                text = json.loads(self.rec.Result())['text']
                 if text != "":
                     return text
 
 
-class Rage():
+class Rage(Voice):
 
     power = False
-    base = Base()
+
+    def __init__(self):
+        while not self.power:
+            command = self.speech_record()
+            start_commands = ("робот", "robot", "rage", "рэй", "меня слышно",)
+            end_commands = ("отключайся", "отключись", "выключись",)
+            print(f"Человек: {command}")
+
+            if command in start_commands:
+                self.input()
+            elif command in end_commands:
+                self.output()
+                break
 
     def input(self):
         self.power = True
         answer_record = 'voice/welcome.wav'
         module.play(answer_record)
-        self.base.waiting(answer_record)
-
-        command = Voice()
+        self.waiting(answer_record)
 
         while self.power:
 
-            text = command.speech_record()
+            command = self.speech_record()
 
-            if text == "перестань слушать":
-                self.power = False
-
-            elif text == "хочешь научиться":
-                command.add_answer()
-
-            elif text != None:
-                print(f"Человек: {text}")
-                command.get_answer(text)
-
-            elif text == None:
+            if command:
+                print(f"Человек: {command}")
+                self.get_answer(command)
+            elif not command:
                 module.play('voice/ask.wav')
                 time.sleep(3)
+            elif command == "перестань слушать":
+                self.power = False
+            elif command == "хочешь научиться":
+                self.add_answer()
 
     def output(self):
         answer_record = 'voice/out.wav'
         module.play(answer_record)
-        self.base.waiting(answer_record)
+        self.waiting(answer_record)
 
 
 if __name__ == '__main__':
 
-    model = Model("speech_to_text")
-    rec = KaldiRecognizer(model, 16000)
-
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=1,
-                    rate=16000,
-                    input=True,
-                    frames_per_buffer=8000)
-    stream.start_stream()
-
     rage = Rage()
-    command = Voice()
-
-    while not rage.power:
-
-        text = command.speech_record()
-        print(f"Человек: {text}")
-
-        if text == 'rage' or text == 'рэй' or text == 'меня слышно':
-            rage.input()
-
-        elif text == 'отключайся':
-            rage.output()
-            break
