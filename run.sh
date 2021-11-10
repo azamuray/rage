6c42f706d1228583fca7a585cdfff00a38ed66d6
Rage <azamat.murdalov@mail.ru>
2021-11-11 01:58:31 +0300

    update entrypoint

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
-sudo apt install python3-pip portaudio19-dev
+# Выбор установки для разных ОС
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  sudo apt install python3-pip portaudio19-dev
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  brew install python3-pip portaudio
+else
+  echo "Failed to determine the OS"
+fi
+
+# Установка python библиотек
+python3 -m pip install --upgrade pip
 pip3 install -r requirements.txt
+
+# Запуск
 python3 main.py
